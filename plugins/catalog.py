479ef5774723ddef2b3dd55f97e5911690673f02
novus42 <novus42@gmail.com>
2010-09-12 15:29:04 +0200

    Add help support. Add capalog plugin.
    Conflicts:
    
    	plugins/catalog.py

--- plugins/catalog.py
+++ plugins/catalog.py
@@ -93,6 +93,7 @@ aniget <number> <string>:
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         string = unicode(string).encode('utf-8')
         string = re.sub(ur"&quot;", ur"\"", string)
         string = re.sub(ur"&#37;", ur"%", string)
@@ -110,6 +111,8 @@ aniget <number> <string>:
 >>>>>>> 7cc59dd... Add help support. Add capalog plugin.
 =======
 >>>>>>> 7cc59dd... Add help support. Add capalog plugin.
+=======
+>>>>>>> 7cc59dd... Add help support. Add capalog plugin.
         string = re.sub(r"&quot;", r"\"", string)
         string = re.sub(r"&#37;", r"%", string)
         string = re.sub(r"&#39; ", r"'", string)        
@@ -123,6 +126,9 @@ aniget <number> <string>:
         string = re.sub(r"&amp;", r"&", string)
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 7cc59dd... Add help support. Add capalog plugin.
+=======
 >>>>>>> 7cc59dd... Add help support. Add capalog plugin.
 =======
 >>>>>>> 7cc59dd... Add help support. Add capalog plugin.
